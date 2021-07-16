0e2fd0ba7b5b0de9874e0c82ae75786f7f3db635
David Sanchez <david.sanchez@kueski.com>
2021-07-15 21:42:56 -0500

    Add initial router
    I know this is a simple application but we need something that helps
    us to manage our routes, right? Here is where the router comes to the
    action.

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
+require_relative './router'
+
 module StockProfitfy
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/plain'}, ['Hello']]
+      request = Rack::Request.new(env)
+      routing(request)
+    end
+
+    def routing(request)
+      Router.new(request).route!
     end
   end
 end
